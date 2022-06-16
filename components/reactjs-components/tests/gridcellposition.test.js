507f75594eef086a2a8fe6c90ecaab62b7d1a52f
Morel Sébastien <morel.seb@gmail.com>
2022-06-15 18:45:03 -0700

    Fix Tests

--- components/reactjs-components/tests/gridcellposition.test.js
+++ components/reactjs-components/tests/gridcellposition.test.js
@@ -10,7 +10,7 @@ test('Get a Grid and check the position', async () => {
     const query = {
         grid: {
             __args: {
-                id: '62a23c5e860bd16784ae5bec',
+                id: '626ea0599161f671155db9a9',
                 language: 'en',
             },
             id: true,
@@ -29,22 +29,18 @@ test('Get a Grid and check the position', async () => {
         },
     };
     const grid = (await fetch(query)).grid;
+
     expect(grid.rows[0].columns[0].layout.rowspan).toBe(1);
-    expect(grid.rows[1].columns[0].layout.rowspan).toBe(3);
+    expect(grid.rows[1].columns[0].layout.rowspan).toBe(1);
+    expect(grid.rows[5].columns[0].layout.rowspan).toBe(2);
     const gridCells = wrapCellsInRows(grid.rows);
 
     const check = ([cellRowIndex, cellColIndex], [rowIndex, colIndex]) => {
         expect(gridCells[cellRowIndex][cellColIndex].position.rowIndex).toBe(rowIndex);
         expect(gridCells[cellRowIndex][cellColIndex].position.colIndex).toBe(colIndex);
     };
-    check([2, 0], [2, 1]);
-    check([2, 1], [2, 3]);
-    check([5, 3], [5, 3]);
-    check([4, 2], [4, 3]);
 
-    check([6, 0], [6, 0]);
-    check([6, 1], [6, 2]);
-
-    check([7, 0], [7, 2]);
-    // console.log(gridCells[2]);
+    check([1, 0], [1, 0]);
+    check([2, 0], [2, 0]);
+    check([6, 0], [6, 2]);
 });
