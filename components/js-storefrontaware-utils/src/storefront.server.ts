b8247c346588dfdbb7eb60c0ea12c8e3d56eb0e0
Raymond Julin <raymond.julin@gmail.com>
2022-07-28 13:43:45 +0200

    chore: Prettier

--- components/js-storefrontaware-utils/src/storefront.server.ts
+++ components/js-storefrontaware-utils/src/storefront.server.ts
@@ -2,23 +2,23 @@ import { createClient } from '@crystallize/js-api-client';
 import { TStoreFront, TStoreFrontAdapter } from './types';
 
 export type StoreFrontOptions = {
-    withSecrets?: boolean
-}
+    withSecrets?: boolean;
+};
 
 /**
-* Create store TStoreFront
-* Load configuration from adapter and configure an apiClient for the store front
-*
-* @param adapter A TStoreFrontAdapter instance
-* @param options Object with options, or the deprecated syntax of a boolean for just `withSecrets`
-* @returns The loaded config + a configured api client
-*/
+ * Create store TStoreFront
+ * Load configuration from adapter and configure an apiClient for the store front
+ *
+ * @param adapter A TStoreFrontAdapter instance
+ * @param options Object with options, or the deprecated syntax of a boolean for just `withSecrets`
+ * @returns The loaded config + a configured api client
+ */
 export const createStoreFront = async (
     adapter: TStoreFrontAdapter,
     options: StoreFrontOptions | boolean = false,
 ): Promise<TStoreFront> => {
-    options = typeof options === "boolean" ? { withSecrets: options } : options
-    const withSecrets = options?.withSecrets ?? false
+    options = typeof options === 'boolean' ? { withSecrets: options } : options;
+    const withSecrets = options?.withSecrets ?? false;
 
     const config = await adapter.config(withSecrets);
     return {
