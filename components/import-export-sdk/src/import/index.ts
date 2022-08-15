95aa7d1d6f76730e121526318c1c6b700ea0412a
Brendan Goodenough <brendan@goodenough.nz>
2022-08-15 12:33:15 +0200

    chore(components/import-export-sdk): Remove import folder

--- components/import-export-sdk/src/import/index.ts
+++
@@ -1,2 +0,0 @@
-export * from './bootstrapper';
-export * from './types';
--- components/import-export-sdk/src/import/schema/index.ts
+++
@@ -1,8 +0,0 @@
-import { z } from 'zod';
-import { ShapeSchema } from '../../schema/shape';
-import { TopicSchema } from '../../schema/topic';
-
-export const ImportSpecSchema = z.object({
-    shapes: z.array(ShapeSchema).optional(),
-    topics: z.array(TopicSchema).optional(),
-});
