a4548b667ad1ab8e995c374f6b74eff927f663d7
Morel Sébastien <morel.seb@gmail.com>
2022-10-04 14:41:41 -0700

    feat(js-storefrontaware-utils): Fix typo

--- components/js-storefrontaware-utils/src/cypher.ts
+++ components/js-storefrontaware-utils/src/cypher.ts
@@ -9,14 +9,14 @@ export function encryptValue(value: string, secretKey: string, algorithm: string
 }
 
 export function decryptValue(value: string, secretKey: string, algorithm: string): string {
-    if (value.includes(':')) {
+    if (value?.includes(':')) {
         const [initVector, encryptedData] = value.split(':');
         const decipher = crypto.createDecipheriv(algorithm, secretKey, Buffer.from(initVector, 'hex'));
         let decryptedData = decipher.update(encryptedData, 'hex', 'utf-8');
         decryptedData += decipher.final('utf8');
         return decryptedData;
     }
-    return value;
+    return value || '';
 }
 
 /**
