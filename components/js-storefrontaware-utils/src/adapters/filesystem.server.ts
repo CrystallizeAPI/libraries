171cc934b0b391b190062c0be462609a70bb8a92
Morel Sébastien <morel.seb@gmail.com>
2022-10-04 13:21:56 -0700

    feat(js-storefrontaware-utils): Enforce empty crystallize secrets

--- components/js-storefrontaware-utils/src/adapters/filesystem.server.ts
+++ components/js-storefrontaware-utils/src/adapters/filesystem.server.ts
@@ -5,7 +5,18 @@ export const createFilesystemAdapter = (filename: string): TStoreFrontAdapter =>
     return {
         config: async (withSecrets: boolean): Promise<TStoreFrontConfig> => {
             const data = await fs.readFile(filename, { encoding: 'utf8' });
-            return JSON.parse(data);
+            const unfilteredSecrets = JSON.parse(data);
+            if (withSecrets) {
+                return unfilteredSecrets;
+            }
+            return {
+                ...unfilteredSecrets,
+                configuration: {
+                    ...unfilteredSecrets.configuration,
+                    ACCESS_TOKEN_ID: '',
+                    ACCESS_TOKEN_SECRET: '',
+                },
+            };
         },
     };
 };
--- components/js-storefrontaware-utils/src/adapters/memory.server.ts
+++ components/js-storefrontaware-utils/src/adapters/memory.server.ts
@@ -2,6 +2,18 @@ import { TStoreFrontAdapter, TStoreFrontConfig } from '../types';
 
 export const createMemoryAdapter = (config: TStoreFrontConfig): TStoreFrontAdapter => {
     return {
-        config: async (withSecrets: boolean): Promise<TStoreFrontConfig> => config,
+        config: async (withSecrets: boolean): Promise<TStoreFrontConfig> => {
+            if (withSecrets) {
+                return config;
+            }
+            return {
+                ...config,
+                configuration: {
+                    ...config.configuration,
+                    ACCESS_TOKEN_ID: '',
+                    ACCESS_TOKEN_SECRET: '',
+                },
+            };
+        },
     };
 };
