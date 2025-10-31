078d7143026602d6e3a2f50996b7c28d395b5179
Morel Sébastien <morel.seb@gmail.com>
2025-10-31 15:24:19 -0700

    fix(schema): fix richText input

--- components/schema/src/pim/components/types/rich-text.ts
+++ components/schema/src/pim/components/types/rich-text.ts
@@ -14,24 +14,8 @@ export const RichTextContentSchema = z.object({
 });
 export type RichTextContent = z.infer<typeof RichTextContentSchema>;
 
-export const RichTextContentInputSchema = z
-    .object({
-        html: z.array(z.string()).optional(),
-        json: z.array(z.string()).optional(),
-    })
-    .refine(
-        ({ json }) => {
-            if (json) {
-                try {
-                    json.map((str) => JSON.parse(str));
-                } catch (err) {
-                    return false;
-                }
-            }
-            return true;
-        },
-        {
-            error: 'Invalid json provided to rich text content input',
-        },
-    );
+export const RichTextContentInputSchema = z.object({
+    html: z.array(z.string()).optional(),
+    json: z.array(z.any()).optional(),
+});
 export type RichTextContentInput = z.infer<typeof RichTextContentInputSchema>;
