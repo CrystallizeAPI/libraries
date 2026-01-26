ca6107d660c1be0fd1bc8e0f05c86a924df93dc4
Pedro Miranda <me@pedromiranda.dev>
2026-01-26 16:25:30 +0000

    feat(mass-operations): add resource identifier to RegisterImageOperationSchema

--- components/schema/src/mass-operation/asset.ts
+++ components/schema/src/mass-operation/asset.ts
@@ -1,10 +1,12 @@
 import { z } from 'zod';
 import { RegisterImageInputSchema } from '../pim';
 import { RefSchema } from '../shared';
+import { ResourceIdentifierSchema } from '../shared';
 
 export const RegisterImageOperationSchema = RegisterImageInputSchema.extend({
     intent: z.literal('image/register'),
     _ref: RefSchema.optional(),
+    resourceIdentifier: ResourceIdentifierSchema.optional(),
 });
 
 export type RegisterImageOperation = z.infer<typeof RegisterImageOperationSchema>;
